cf8d3d674e25ae91f63a12ed2346da1796b4387c
LaNterNAloNe <118646991+LaNterNAloNe@users.noreply.github.com>
2025-03-10 21:22:07 +0800

    0.1.16

--- SOURCE/MAIN.C
+++ SOURCE/MAIN.C
@@ -1,8 +1,15 @@
 #include"GLOBAL.H"
+
+void drawExittingProgram(void){
+    setfillstyle(SOLID_FILL, CYAN);
+    bar(120,120,520,390);
+    puthz(180, 200, "你正在退出程序，按任意键退出", 24, 20, MY_WHITE);
+}
  
 void main(){
-    int page=LOGIN;
+    int page=MAIN_ADMIN;
     // 如果想测试函数，将page设为TEST //如果想正常执行程序，将page设为LOGIN
+    int driver=VGA,mode=VGAHI;
 
     initgraph(&driver, &mode, "C:\\BORLANDC\\BGI");
     cleardevice();
@@ -19,7 +26,12 @@ void main(){
                 _register(&page);break;
             case TEST:
                 break;
-            // MAIN_GRAPH.C
+            // USER_MAIN_GRAPH.C
+
+            //ADMIN_MAIN_GRAPH.C
+            case MAIN_ADMIN:
+                main_admin(&page);break;
+            
         }
     }
     clrmous(MouseX,MouseY);
---
+++ SOURCE/_ADMIN_.C
@@ -0,0 +1,68 @@
+#include "GLOBAL.H"
+
+
+void main_admin(int *page){
+    clrmous(MouseX, MouseY);
+    drawgraph_admin_login();
+    mouseinit();
+
+    while(1){
+        newmouse(&MouseX, &MouseY, &press);
+        if (mouse_press(0, 0, 640, 480) == 1) {
+            *page = EXIT;
+            return;
+        }
+    }   
+
+}
+
+
+void drawgraph_admin_login(void){
+    /*** 绘制菜单 ***/
+    setfillstyle(SOLID_FILL, CYAN);
+    bar(MENU_X1, MENU_Y1, MENU_X2, MENU_Y2);
+    setcolor(BLUE);
+    setlinestyle(SOLID_LINE, 0, THICK_WIDTH);
+    line(MENU_X1+2, MENU_Y1+2, MENU_X2-2, MENU_Y1+2);
+    line(MENU_X1+2, MENU_Y1+2, MENU_X1+2, MENU_Y2-2);
+    line(MENU_X1+2, MENU_Y2-2, MENU_X2-2, MENU_Y2-2);
+    line(MENU_X2-2, MENU_Y1+2, MENU_X2-2, MENU_Y2-2);
+    setcolor(MY_BLACK);
+    setlinestyle(SOLID_LINE, 0, NORM_WIDTH);
+    line(MENU_X1+2, MENU_Y1+2, MENU_X2-2, MENU_Y1+2);
+    line(MENU_X1+2, MENU_Y1+2, MENU_X1+2, MENU_Y2-2);
+    line(MENU_X1+2, MENU_Y2-2, MENU_X2-2, MENU_Y2-2);
+    line(MENU_X2-2, MENU_Y1+2, MENU_X2-2, MENU_Y2-2);
+
+    /*** 绘制交互界面A（暂为空白） ***/
+    setfillstyle(SOLID_FILL, BLUE);
+    bar(INTERFACE_A_X1, INTERFACE_A_Y1, INTERFACE_A_X2, INTERFACE_A_Y2);
+    setcolor(CYAN);
+    setlinestyle(SOLID_LINE, 0, THICK_WIDTH);
+    line(INTERFACE_A_X1+2, INTERFACE_A_Y1+2, INTERFACE_A_X2-2, INTERFACE_A_Y1+2);
+    line(INTERFACE_A_X1+2, INTERFACE_A_Y1+2, INTERFACE_A_X1+2, INTERFACE_A_Y2-2);
+    line(INTERFACE_A_X1+2, INTERFACE_A_Y2-2, INTERFACE_A_X2-2, INTERFACE_A_Y2-2);
+    line(INTERFACE_A_X2-2, INTERFACE_A_Y1+2, INTERFACE_A_X2-2, INTERFACE_A_Y2-2);
+    setcolor(MY_LIGHTGRAY);
+    setlinestyle(SOLID_LINE, 0, NORM_WIDTH);
+    line(INTERFACE_A_X1+2, INTERFACE_A_Y1+2, INTERFACE_A_X2-2, INTERFACE_A_Y1+2);
+    line(INTERFACE_A_X1+2, INTERFACE_A_Y1+2, INTERFACE_A_X1+2, INTERFACE_A_Y2-2);
+    line(INTERFACE_A_X1+2, INTERFACE_A_Y2-2, INTERFACE_A_X2-2, INTERFACE_A_Y2-2);
+    line(INTERFACE_A_X2-2, INTERFACE_A_Y1+2, INTERFACE_A_X2-2, INTERFACE_A_Y2-2);
+
+    /*** 绘制交互界面B（暂为空白） ***/
+    setfillstyle(SOLID_FILL, BLUE);
+    bar(INTERFACE_B_X1, INTERFACE_B_Y1, INTERFACE_B_X2, INTERFACE_B_Y2);
+    setcolor(CYAN);
+    setlinestyle(SOLID_LINE, 0, THICK_WIDTH);
+    line(INTERFACE_B_X1+2, INTERFACE_B_Y1+2, INTERFACE_B_X2-2, INTERFACE_B_Y1+2);
+    line(INTERFACE_B_X1+2, INTERFACE_B_Y1+2, INTERFACE_B_X1+2, INTERFACE_B_Y2-2);
+    line(INTERFACE_B_X1+2, INTERFACE_B_Y2-2, INTERFACE_B_X2-2, INTERFACE_B_Y2-2);
+    line(INTERFACE_B_X2-2, INTERFACE_B_Y1+2, INTERFACE_B_X2-2, INTERFACE_B_Y2-2);
+    setcolor(MY_LIGHTGRAY);
+    setlinestyle(SOLID_LINE, 0, NORM_WIDTH);
+    line(INTERFACE_B_X1+2, INTERFACE_B_Y1+2, INTERFACE_B_X2-2, INTERFACE_B_Y1+2);
+    line(INTERFACE_B_X1+2, INTERFACE_B_Y1+2, INTERFACE_B_X1+2, INTERFACE_B_Y2-2);
+    line(INTERFACE_B_X1+2, INTERFACE_B_Y2-2, INTERFACE_B_X2-2, INTERFACE_B_Y2-2);
+    line(INTERFACE_B_X2-2, INTERFACE_B_Y1+2, INTERFACE_B_X2-2, INTERFACE_B_Y2-2);
+}
